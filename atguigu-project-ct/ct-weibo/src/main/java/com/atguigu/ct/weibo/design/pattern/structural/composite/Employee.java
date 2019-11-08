ee4194202222d5a57bc46d323cc263e29ca526ae
wangbin <392328206@qq.com>
2019-11-08 16:01:00 +0800

    组合模式 类似于 类目 树结构的封装

---
+++ atguigu-project-ct/ct-weibo/src/main/java/com/atguigu/ct/weibo/design/pattern/structural/composite/Employee.java
@@ -0,0 +1,39 @@
+package com.atguigu.ct.weibo.design.pattern.structural.composite;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+    //构造函数
+    public Employee() {
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+
+}
