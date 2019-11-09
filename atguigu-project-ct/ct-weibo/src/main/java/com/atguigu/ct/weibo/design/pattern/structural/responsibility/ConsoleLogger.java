12dec24acb96c7b8ddf3913825e6c88c67cb2236
wangbin <392328206@qq.com>
2019-11-09 22:25:57 +0800

    责任链模式 一层跟着一层 类似于 zuul 对于请求的过滤 可以用这个分为 h5 web app 分别定制过滤规则

---
+++ atguigu-project-ct/ct-weibo/src/main/java/com/atguigu/ct/weibo/design/pattern/structural/responsibility/ConsoleLogger.java
@@ -0,0 +1,13 @@
+package com.atguigu.ct.weibo.design.pattern.structural.responsibility;
+
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+}
