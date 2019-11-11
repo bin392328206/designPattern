b8679769d84a736919cf531ecda84b157b970198
wangbin <392328206@qq.com>
2019-11-11 14:50:24 +0800

    观察者模式

---
+++ atguigu-project-ct/ct-weibo/src/main/java/com/atguigu/ct/weibo/design/pattern/structural/observer/BinaryObserver.java
@@ -0,0 +1,15 @@
+package com.atguigu.ct.weibo.design.pattern.structural.observer;
+
+public class BinaryObserver extends Observer{
+
+    public BinaryObserver(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println( "Binary String: "
+                + Integer.toBinaryString( subject.getState() ) );
+    }
+}
