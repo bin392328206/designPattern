45c605a4fc2a4fb9b965bf90cd6c2cc817178fae
wangbin <392328206@qq.com>
2019-11-11 13:15:58 +0800

    中介着模式

---
+++ atguigu-project-ct/ct-weibo/src/main/java/com/atguigu/ct/weibo/design/pattern/structural/mediator/User.java
@@ -0,0 +1,21 @@
+package com.atguigu.ct.weibo.design.pattern.structural.mediator;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name){
+        this.name  = name;
+    }
+
+    public void sendMessage(String message){
+        ChatRoom.showMessage(this,message);
+    }
+}
